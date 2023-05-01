daa8f38ce1a911d8cf4a7c5cf897197e39f74c54
Lord, Acacia [USA] <Lord_Acacia@bah.com>
2023-05-01 11:03:17 -0400

    Correct way - Fetching Route Paramters Relatively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +16,17 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    //params is an Observable, allowing you to work with asynchronous tasks.
+    //An Observable allows you to subscribe to an event that MIGHT happen in the future, without having to wait for it
+    this.route.params.subscribe(
+      //Checks for params changes
+      (params: Params) => {
+        //What will be executed when params changes
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+
+      }
+    );
   }
 
 }
