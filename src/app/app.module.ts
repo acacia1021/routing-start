3ec7acb145632f0dfaca1aa9cabecc761e5e9a39
Lord, Acacia [USA] <Lord_Acacia@bah.com>
2023-05-12 10:15:04 -0400

    Outsourcing Route Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,57 +13,61 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  //Example for localhost:4200/users
-  // {
-  //   path: 'users', component: UsersComponent
-  // },
-  {
-    path: '', component: HomeComponent
-  },
-  {
-    path: 'users', component: UsersComponent, children: [
-      {
-        path: ':id/:name', component: UserComponent
-      },
-    ]
-  },
-  // {
-  //   path: 'users/:id/:name', component: UserComponent
-  // },
+// const appRoutes: Routes = [
+//   //TYPICALLY, if you have more than 3 routes, you will create an entirely new file to hold the routes
 
-  {
-    path: 'servers', component: ServersComponent, children: [
-      {
-        path: ':id', component: ServerComponent
-      },
-      {
-        path: ':id/edit', component: EditServerComponent
-      }
-    ]
-  },
-  // {
-  //   path: 'servers/:id', component: ServerComponent
-  // },
-  // {
-  //   path: 'servers/:id/edit', component: EditServerComponent
-  // },
-  {
-    path: 'not-found', component: PageNotFoundComponent
-  },
-  {
-    path: 'something', redirectTo: '/not-found'
-  },
 
-  // //You would need to add the pathMatch: part if your main path will collide with another
-  // { path: '', redirectTo: '/somewhere-else', pathMatch: 'full' },
+//   //Example for localhost:4200/users
+//   // {
+//   //   path: 'users', component: UsersComponent
+//   // },
+//   {
+//     path: '', component: HomeComponent
+//   },
+//   {
+//     path: 'users', component: UsersComponent, children: [
+//       {
+//         path: ':id/:name', component: UserComponent
+//       },
+//     ]
+//   },
+//   // {
+//   //   path: 'users/:id/:name', component: UserComponent
+//   // },
 
-  //This is the catch all wildcard for paths -- needs to be the last one in the array of routes, as the routes are parsed from top to bottom
-  {
-    path: '**', redirectTo: '/not-found'
-  }
-];
+//   {
+//     path: 'servers', component: ServersComponent, children: [
+//       {
+//         path: ':id', component: ServerComponent
+//       },
+//       {
+//         path: ':id/edit', component: EditServerComponent
+//       }
+//     ]
+//   },
+//   // {
+//   //   path: 'servers/:id', component: ServerComponent
+//   // },
+//   // {
+//   //   path: 'servers/:id/edit', component: EditServerComponent
+//   // },
+//   {
+//     path: 'not-found', component: PageNotFoundComponent
+//   },
+//   {
+//     path: 'something', redirectTo: '/not-found'
+//   },
+
+//   // //You would need to add the pathMatch: part if your main path will collide with another
+//   // { path: '', redirectTo: '/somewhere-else', pathMatch: 'full' },
+
+//   //This is the catch all wildcard for paths -- needs to be the last one in the array of routes, as the routes are parsed from top to bottom
+//   {
+//     path: '**', redirectTo: '/not-found'
+//   }
+// ];
 
 @NgModule({
   declarations: [
@@ -79,7 +83,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    // RouterModule.forRoot(appRoutes),
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
